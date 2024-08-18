e050c272cea64f94574fee5309c4c538b8bd3870
Acelya Lejeune <145855362+LejeuneA@users.noreply.github.com>
2024-08-18 19:12:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+// Detecting Button Press
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+
+        switch (buttonInnerHTML) {
+            case "w":
+                var audio = new Audio("../Drum Kit Starting Files/sounds/tom-4.mp3");
+                audio.play();
+                break;
+
+            case "a":
+                var audio = new Audio("../Drum Kit Starting Files/sounds/tom-3.mp3");
+                audio.play();
+                break;
+
+            case "s":
+                var audio = new Audio("../Drum Kit Starting Files/sounds/tom-2.mp3");
+                audio.play();
+                break;
+
+            case "d":
+                var audio = new Audio("../Drum Kit Starting Files/sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "j":
+                var audio = new Audio("../Drum Kit Starting Files/sounds/snare.mp3");
+                audio.play();
+                break;
+
+            case "k":
+                var audio = new Audio("../Drum Kit Starting Files/sounds/kick-bass.mp3");
+                audio.play();
+                break;
+
+            case "l":
+                var audio = new Audio("../Drum Kit Starting Files/sounds/crash.mp3");
+                audio.play();
+                break;
+
+            default: console.log (buttonInnerHTML);
+                break;
+        }
+    });
+};
+
+
+// Detecting Keyboard Press
+document.addEventListener ("keydown", function (event) {
+    makeSound(event.key);
+});
+
+
+function makeSound (key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("../Drum Kit Starting Files/sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        case "a":
+            var audio = new Audio("../Drum Kit Starting Files/sounds/tom-3.mp3");
+            audio.play();
+            break;
+
+        case "s":
+            var audio = new Audio("../Drum Kit Starting Files/sounds/tom-2.mp3");
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio("../Drum Kit Starting Files/sounds/tom-1.mp3");
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio("../Drum Kit Starting Files/sounds/snare.mp3");
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio("../Drum Kit Starting Files/sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+        case "l":
+            var audio = new Audio("../Drum Kit Starting Files/sounds/crash.mp3");
+            audio.play();
+            break;
+
+        default: console.log (buttonInnerHTML);
+            break;
+    }
+}
